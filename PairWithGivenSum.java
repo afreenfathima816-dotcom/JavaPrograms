cf423b430e8b5743246f489ddd7bf0ffb71ab936
afreenfathima816-dotcom <afreenfathima816@gmail.com>
2026-07-23 17:08:08 +0400

    Implement pair finding for given sum in an array
    This program reads an array of integers and a target sum, then finds and prints a pair of numbers that add up to the target. If no such pair exists, it outputs 'Pair not found'.

---
+++ PairWithGivenSum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PairWithGivenSum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Pair not found");
+        }
+
+        sc.close();
+    }
+}
