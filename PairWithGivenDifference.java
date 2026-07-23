083ad686116eba6fb1f90c84037e222cede8b3be
afreenfathima816-dotcom <afreenfathima816@gmail.com>
2026-07-23 17:08:47 +0400

    Implement pair finding with given difference

---
+++ PairWithGivenDifference.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PairWithGivenDifference {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(arr[i] - arr[j]) == k) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Pair not found");
+        }
+
+        sc.close();
+    }
+}
