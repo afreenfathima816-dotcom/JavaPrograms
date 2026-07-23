bfa21a970c90cd9e68274c497a1b2786521f3568
afreenfathima816-dotcom <afreenfathima816@gmail.com>
2026-07-23 17:06:41 +0400

    Add SubArraySum program to find target subarray
    Implement a Java program to find a subarray with a given sum.

---
+++ SubArraySum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SubArraySum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (sum == target) {
+                    System.out.println("Subarray found from index " + i + " to " + j);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Subarray not found");
+        }
+
+        sc.close();
+    }
+}
