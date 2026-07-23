3da918560385e795faa59dc962377dbce737757a
afreenfathima816-dotcom <afreenfathima816@gmail.com>
2026-07-23 17:10:05 +0400

    Implement triplet sum to zero finder
    This program reads an integer array and finds all unique triplets that sum to zero, printing them to the console. If no such triplets are found, it outputs a corresponding message.

---
+++ TripletsSumZero.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TripletsSumZero {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean found = false;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.println(arr[i] + " " + arr[j] + " " + arr[k]);
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No triplets found");
+        }
+
+        sc.close();
+    }
+}
